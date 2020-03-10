4633a766784b5d9be82a0aadd491f854ef65fe4d
claudios <claudio.siegenthaler@zebbra.ch>
2020-03-10 14:18:13 +0100

    supress ssr for codemirror editor due to missing navigator element during ssr

--- lib/components/Editor.vue
+++ lib/components/Editor.vue
@@ -9,70 +9,76 @@
       </div>
     </div>
     <div>
-      <codemirror
-        :value="value"
-        :readonly="readonly"
-        :options="options"
-        @input="$emit('input', $event)"
-      />
+      <no-ssr>
+        <codemirror
+          :value="value"
+          :readonly="readonly"
+          :options="options"
+          @input="$emit('input', $event)"
+        />
+      </no-ssr>
     </div>
   </div>
 </template>
 
 <script>
-import { codemirror } from 'vue-codemirror'
-import 'codemirror/lib/codemirror.css'
+import { codemirror } from "vue-codemirror";
+import NoSSR from "vue-no-ssr";
+import "codemirror/lib/codemirror.css";
 // language
-import 'codemirror/mode/jinja2/jinja2.js'
-import 'codemirror/mode/javascript/javascript.js'
+import "codemirror/mode/jinja2/jinja2.js";
+import "codemirror/mode/javascript/javascript.js";
 
 // active-line.js
-import 'codemirror/addon/selection/active-line.js'
+import "codemirror/addon/selection/active-line.js";
 
 // styleSelectedText
-import 'codemirror/addon/selection/mark-selection.js'
-import 'codemirror/addon/search/searchcursor.js'
+import "codemirror/addon/selection/mark-selection.js";
+import "codemirror/addon/search/searchcursor.js";
 
 // highlightSelectionMatches
-import 'codemirror/addon/scroll/annotatescrollbar.js'
-import 'codemirror/addon/search/matchesonscrollbar.js'
+import "codemirror/addon/scroll/annotatescrollbar.js";
+import "codemirror/addon/search/matchesonscrollbar.js";
 
-import 'codemirror/addon/search/match-highlighter.js'
+import "codemirror/addon/search/match-highlighter.js";
 
 // keyMap
-import 'codemirror/mode/clike/clike.js'
-import 'codemirror/addon/edit/matchbrackets.js'
-import 'codemirror/addon/comment/comment.js'
-import 'codemirror/addon/dialog/dialog.js'
-import 'codemirror/addon/dialog/dialog.css'
+import "codemirror/mode/clike/clike.js";
+import "codemirror/addon/edit/matchbrackets.js";
+import "codemirror/addon/comment/comment.js";
+import "codemirror/addon/dialog/dialog.js";
+import "codemirror/addon/dialog/dialog.css";
 
-import 'codemirror/addon/search/search.js'
-import 'codemirror/keymap/sublime.js'
+import "codemirror/addon/search/search.js";
+import "codemirror/keymap/sublime.js";
 
 // foldGutter
-import 'codemirror/addon/fold/foldgutter.css'
-import 'codemirror/addon/fold/brace-fold.js'
-import 'codemirror/addon/fold/comment-fold.js'
-import 'codemirror/addon/fold/foldcode.js'
-import 'codemirror/addon/fold/foldgutter.js'
-import 'codemirror/addon/fold/indent-fold.js'
-import 'codemirror/addon/fold/markdown-fold.js'
-import 'codemirror/addon/fold/xml-fold.js'
+import "codemirror/addon/fold/foldgutter.css";
+import "codemirror/addon/fold/brace-fold.js";
+import "codemirror/addon/fold/comment-fold.js";
+import "codemirror/addon/fold/foldcode.js";
+import "codemirror/addon/fold/foldgutter.js";
+import "codemirror/addon/fold/indent-fold.js";
+import "codemirror/addon/fold/markdown-fold.js";
+import "codemirror/addon/fold/xml-fold.js";
 
 export default {
-  components: { codemirror },
+  components: {
+    codemirror,
+    "no-ssr": NoSSR
+  },
   props: {
     value: {
       type: String,
-      default: ''
+      default: ""
     },
     language: {
       type: String,
-      default: 'none'
+      default: "none"
     },
     label: {
       type: String,
-      default: 'none'
+      default: "none"
     },
     readonly: {
       type: Boolean,
@@ -87,24 +93,24 @@ export default {
         styleActiveLine: true,
         lineNumbers: true,
         line: true,
-        keyMap: 'sublime',
+        keyMap: "sublime",
         mode: this.language,
         readOnly: this.readonly,
-        theme: 'neo',
+        theme: "neo",
         extraKeys: {
           F11(cm) {
-            cm.setOption('fullScreen', !cm.getOption('fullScreen'))
+            cm.setOption("fullScreen", !cm.getOption("fullScreen"));
           },
           Esc(cm) {
-            if (cm.getOption('fullScreen')) {
-              cm.setOption('fullScreen', false)
+            if (cm.getOption("fullScreen")) {
+              cm.setOption("fullScreen", false);
             }
           }
         }
       }
-    }
+    };
   }
-}
+};
 </script>
 
 <style type="scss">
